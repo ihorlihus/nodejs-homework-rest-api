b9e82bc19a5fcb06c4a16113cf0f57294fee45e3
ihorlihus <ligusigor@ukr.net>
2022-11-17 17:02:39 +0200

    login, token, add contact to user

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const cors = require("cors");
 const contactsRouter = require("./routes/api/contacts.router");
 const authRouter = require("./routes/api/auth.router");
+const userRouter = require("./routes/api/user.router");
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
