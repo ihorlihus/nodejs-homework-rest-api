d2b970a1f2a2a55130fe9ecb9ba1ea2f92c90b5e
ihorlihus <ligusigor@ukr.net>
2022-11-27 17:34:13 +0200

    done, without tests

--- middlewares/avatarForTmp.js
+++ middlewares/avatarForTmp.js
@@ -1,23 +1,4 @@
 const gravatar = require("gravatar");
-// const multer = require("multer");
-// const path = require("path");
-// const { nanoid } = require("nanoid");
-// // const fs = require("fs/promises");
-
-// const storage = multer.diskStorage({
-//   dest: path.join(__dirname, "../tmp"),
-//   filename: function (req, file, cb) {
-//     cb(null, nanoid() + file.originalname);
-//   },
-// });
-
-// const upload = multer({
-//   storage,
-//   // can set limits
-//   limits: {
-//     // fileSize: 1, // in bytes
-//   },
-// });
 
 async function avatarForTmp(req, res, next) {
   const { email } = req.user;
